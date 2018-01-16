83aed4b045c7892ca7443ffbb0ea414a148a29b1
mdinant <matthijs.dinant@gmail.com>
2018-01-16 18:41:05 +0100

    first

---
+++ stack.asm
@@ -0,0 +1,15 @@
+; Here is the definition of our BSS section. Right now, we'll use
+; it just to store the stack. Remember that a stack actually grows
+; downwards, so we declare the size of the data before declaring
+; the identifier '_sys_stack'
+
+;TODO: Get these from include
+%define MAX_CPU 128
+%define STACK_SIZE 0x1000
+
+global end_stack
+
+ALIGN 16
+SECTION .bss
+	resb STACK_SIZE * MAX_CPU               ; This reserves 8KBytes of memory here
+end_stack:
